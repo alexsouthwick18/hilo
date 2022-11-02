6d1f0626a3199854bfd060dabeed13b47860c568
alexsouthwick18 <91078301+alexsouthwick18@users.noreply.github.com>
2022-11-02 09:31:23 -0600

    updated program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace cse21001
+{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
