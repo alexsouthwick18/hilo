f5b4346fdcd321203d2a5afe551c6533044f20da
Gunner White <gunnerwhite00@gmail.com>
2022-11-03 09:47:33 -0600

    doing

--- Director.cs
+++ Director.cs
@@ -1,21 +1,25 @@
+using System;
+
 class Director
 {
+    bool keepPlaying = true;
     public void StartGame()
-{
-    while (keepPlaying)
     {
-        GetInputs();
-        DoUpdates();
-        DoOutputs();
+        while (keepPlaying)
+        {
+            GetInputs();
+            DoUpdates();
+            DoOutputs();
+        }
     }
-}
     public void GetInputs()
     {
-
+        Console.Write("Is the next card going to be Higher or Lower? ");
+        string response = Console.ReadLine();
     }
     public void DoUpdates()
     {
-        
+
     }
     public void DoOutputs()
     {
--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ namespace Hilo
         {
             Director director = new Director();
             director.StartGame();
-
         }
     }
 }
