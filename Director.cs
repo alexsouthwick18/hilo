3380ae540738a277447db2b13809fe13c2979063
Brendan Willis <brendanwillis@me.com>
2022-11-03 09:44:18 -0600

    Get inputs check

--- Director.cs
+++ Director.cs
@@ -2,23 +2,25 @@ class Director
 {
     public void StartGame()
 {
-    while (keepPlaying)
-    {
-        GetInputs();
-        DoUpdates();
-        DoOutputs();
-    }
+        while (keepPlaying)
+        {
+            GetInputs();
+            DoUpdates();
+            DoOutputs();
+        }
 }
-    public void GetInputs()
-    {
-
-    }
-    public void DoUpdates()
-    {
-        
-    }
-    public void DoOutputs()
-    {
-        
-    }
+        public void GetInputs()
+        {
+            Console.WriteLine("Would you like to draw a card? [y/n]");
+            string drawDeck = Console.ReadLine();
+            keepPlaying = (drawDeck == "y")
+        }
+        public void DoUpdates()
+        {
+            
+        }
+        public void DoOutputs()
+        {
+            
+        }
 }
