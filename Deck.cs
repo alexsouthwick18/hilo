aa7e87f6621035d335607378c27ca275cbc2b95e
Gunner White <gunnerwhite00@gmail.com>
2022-11-03 09:26:34 -0600

    I am better than everyone

---
+++ Deck.cs
@@ -0,0 +1,5 @@
+class Deck
+{
+    int[] newArray = new int[13];
+    
+}
---
+++ Director.cs
@@ -0,0 +1,24 @@
+class Director
+{
+    public void StartGame()
+{
+    while (keepPlaying)
+    {
+        GetInputs();
+        DoUpdates();
+        DoOutputs();
+    }
+}
+    public void GetInputs()
+    {
+
+    }
+    public void DoUpdates()
+    {
+        
+    }
+    public void DoOutputs()
+    {
+        
+    }
+}
