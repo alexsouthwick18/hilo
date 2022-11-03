88ff44d69e249b1e7fd16b83799ac23d577b3b06
alexsouthwick18 <91078301+alexsouthwick18@users.noreply.github.com>
2022-11-03 09:54:57 -0600

    please work

--- Director.cs
+++ Director.cs
@@ -19,14 +19,7 @@ class Director
     }
     public void DoUpdates()
     {
-<<<<<<< HEAD
-        for (int i = 0; i < 10; i++)
-        {
-            Console.WriteLine(i);
-        }
-=======
-
->>>>>>> cf601b90baa215fb16ff73cafb1d3c83f81dbaf3
+        
     }
     public void DoOutputs()
     {
