4165df9c995623099cbb063819daa458cd304b62
alexsouthwick18 <91078301+alexsouthwick18@users.noreply.github.com>
2022-11-05 12:58:45 -0600

    doupdates

--- Director.cs
+++ Director.cs
@@ -16,11 +16,18 @@ class Director
     {
         Console.Write("Is the next card going to be Higher or Lower? ");
         string response = Console.ReadLine();
-        keepPlaying = (response == "y");
     }
     public void DoUpdates()
     {
-        
+        Random numberGen = new Random();
+
+        int card = 0;
+
+        while (card == 0) {
+            card = numberGen.Next(1,14);
+            Console.WriteLine();
+        }
+
     }
     public void DoOutputs()
     {
